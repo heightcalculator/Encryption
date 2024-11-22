bae05f270263441caf1983edc52305d1fb0e3ffe
Aarush Lanjharia <91100197+heightcalculator@users.noreply.github.com>
2024-11-22 00:28:08 -0500

    Delete CaesarCipher directory

--- CaesarCipher/CaesarDecryption.js
+++
@@ -1,7 +0,0 @@
-function decrypt(encryptionKey, cipher){
-    message = "";
-    for (let i = 0; i < cipher.length; i++) {
-        message += String.fromCharCode((cipher.charCodeAt(i)-encryptionKey) % 255);
-    }
-    return message;
-}
--- CaesarCipher/CaesarEncryption.js
+++
@@ -1,7 +0,0 @@
-function encrypt(encryptionKey, message){
-    crypticMessage = "";
-    for (let i = 0; i < message.length; i++) {
-        crypticMessage += String.fromCharCode((message.charCodeAt(i)+encryptionKey) % 255);
-    }
-    return crypticMessage;
-}
