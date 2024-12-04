3e454eb9c13a0ed308c4014889f0122a5b35523b
Aarush Lanjharia <91100197+heightcalculator@users.noreply.github.com>
2024-12-04 17:42:26 -0500

    Update encryption.js

--- encryption.js
+++ encryption.js
@@ -78,7 +78,7 @@ function encrypt(message) {
         for (let j = 0; j < Ns; j++) {
             toEncrypt = toEncrypt + messageArr[i + j];
         }
-        let relInt = (BigInt(toEncrypt) ** e) % n;
+        let relInt = modPow(BigInt(toEncrypt), e, n);
         let relInt64 = b.encode(relInt); //Converting Ciphertext number from Base 10 to Base 64
         newString += relInt64;
         newString += "#";
